e212999453591c390fe5c35abccd32c595fa1cc5
edo888 <edo888@e0ad00cc-d266-11dd-a2b5-e5f827957e07>
2008-12-25 17:51:57 +0000

    Added load Editor plugins feature

--- trunk/xinha.php
+++ trunk/xinha.php
@@ -10,49 +10,69 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
 jimport( 'joomla.plugin.plugin' );
 
-/**
- * Xinha WYSIWYG Editor Plugin
- *
- * @author Edvard Ananyan <edo888@gmail.com>
- * @package Editors
- * @since 1.5
- */
 class plgEditorXinha extends JPlugin {
-    /**
-     * Constructor
-     *
-     * For php4 compatability we must not use the __constructor as a constructor for plugins
-     * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
-     * This causes problems with cross-referencing necessary for the observer design pattern.
-     *
-     * @param   object $subject The object to observe
-     * @param   array  $config  An array that holds the plugin configuration
-     * @since 1.5
-     */
-    function plgXinha(& $subject, $config){
+    function plgXinha(&$subject, $config) {
         parent::__construct($subject, $config);
     }
 
-    /**
-     * Method to handle the onInit event.
-     *  - Initializes the Xinha WYSIWYG Editor
-     *
-     * @access public
-     * @return string JavaScript Initialization string
-     * @since 1.5
-     */
     function onInit() {
         // Editor gets loaded twice in Legacy mode
-        if(defined('_XINHA_ISLOADED'))
-            return false;
-
+        if(defined('_XINHA_ISLOADED')) return false;
         define('_XINHA_ISLOADED', 1);
 
         $plugin =& JPluginHelper::getPlugin('editors', 'xinha');
-        //$params = new JParameter($plugin->params);
         $params = $plugin->params;
         $language = $params->get('language', 'en');
         $skin = $params->get('skin', '');
+        $load_plugins = '';
+        if($params->get('plugin_Abbreviation', 0) == 1) $load_plugins .= "'Abbreviation',";
+        if($params->get('plugin_BackgroundImage', 0) == 1) $load_plugins .= "'BackgroundImage',";
+        if($params->get('plugin_CharacterMap', 1) == 1) $load_plugins .= "'CharacterMap',";
+        if($params->get('plugin_CharCounter', 0) == 1) $load_plugins .= "'CharCounter',";
+        if($params->get('plugin_ClientsideSpellcheck', 0) == 1) $load_plugins .= "'ClientsideSpellcheck',";
+        if($params->get('plugin_ContextMenu', 1) == 1) $load_plugins .= "'ContextMenu',";
+        if($params->get('plugin_CSS', 0) == 1) $load_plugins .= "'CSS',";
+        if($params->get('plugin_DefinitionList', 0) == 1) $load_plugins .= "'DefinitionList',";
+        if($params->get('plugin_DoubleClick', 0) == 1) $load_plugins .= "'DoubleClick',";
+        if($params->get('plugin_DynamicCSS', 0) == 1) $load_plugins .= "'DynamicCSS',";
+        if($params->get('plugin_EditTag', 0) == 1) $load_plugins .= "'EditTag',";
+        if($params->get('plugin_Equation', 0) == 1) $load_plugins .= "'Equation',";
+        if($params->get('plugin_ExtendedFileManager', 0) == 1) $load_plugins .= "'ExtendedFileManager',";
+        if($params->get('plugin_Filter', 0) == 1) $load_plugins .= "'Filter',";
+        if($params->get('plugin_FindReplace', 0) == 1) $load_plugins .= "'FindReplace',";
+        if($params->get('plugin_FormOperations', 0) == 1) $load_plugins .= "'FormOperations',";
+        if($params->get('plugin_Forms', 0) == 1) $load_plugins .= "'Forms',";
+        if($params->get('plugin_FullPage', 0) == 1) $load_plugins .= "'FullPage',";
+        if($params->get('plugin_GenericPlugin', 0) == 1) $load_plugins .= "'GenericPlugin',";
+        if($params->get('plugin_GetHtml', 0) == 1) $load_plugins .= "'GetHtml',";
+        if($params->get('plugin_HorizontalRule', 0) == 1) $load_plugins .= "'HorizontalRule',";
+        if($params->get('plugin_HtmlEntities', 0) == 1) $load_plugins .= "'HtmlEntities',";
+        if($params->get('plugin_HtmlTidy', 0) == 1) $load_plugins .= "'HtmlTidy',";
+        if($params->get('plugin_ImageManager', 1) == 1) $load_plugins .= "'ImageManager',";
+        if($params->get('plugin_InsertAnchor', 0) == 1) $load_plugins .= "'InsertAnchor',";
+        if($params->get('plugin_InsertMarquee', 0) == 1) $load_plugins .= "'InsertMarquee',";
+        if($params->get('plugin_InsertPagebreak', 0) == 1) $load_plugins .= "'InsertPagebreak',";
+        if($params->get('plugin_InsertPicture', 0) == 1) $load_plugins .= "'InsertPicture',";
+        if($params->get('plugin_InsertSmiley', 0) == 1) $load_plugins .= "'InsertSmiley',";
+        if($params->get('plugin_InsertSnippet', 0) == 1) $load_plugins .= "'InsertSnippet',";
+        if($params->get('plugin_InsertWords', 0) == 1) $load_plugins .= "'InsertWords',";
+        if($params->get('plugin_LangMarks', 0) == 1) $load_plugins .= "'LangMarks',";
+        if($params->get('plugin_Linker', 1) == 1) $load_plugins .= "'Linker',";
+        if($params->get('plugin_ListType', 1) == 1) $load_plugins .= "'ListType',";
+        if($params->get('plugin_NoteServer', 0) == 1) $load_plugins .= "'NoteServer',";
+        if($params->get('plugin_PasteText', 0) == 1) $load_plugins .= "'PasteText',";
+        if($params->get('plugin_PreserveScripts', 0) == 1) $load_plugins .= "'PreserveScripts',";
+        if($params->get('plugin_QuickTag', 0) == 1) $load_plugins .= "'QuickTag',";
+        if($params->get('plugin_SaveSubmit', 0) == 1) $load_plugins .= "'SaveSubmit',";
+        if($params->get('plugin_SetId', 0) == 1) $load_plugins .= "'SetId',";
+        if($params->get('plugin_SmartReplace', 0) == 1) $load_plugins .= "'SmartReplace',";
+        if($params->get('plugin_SpellChecker', 0) == 1) $load_plugins .= "'SpellChecker',";
+        if($params->get('plugin_Stylist', 1) == 1) $load_plugins .= "'Stylist',";
+        if($params->get('plugin_SuperClean', 1) == 1) $load_plugins .= "'SuperClean',";
+        if($params->get('plugin_TableOperations', 0) == 1) $load_plugins .= "'TableOperations',";
+        if($params->get('plugin_Template', 0) == 1) $load_plugins .= "'Template',";
+        if($params->get('plugin_UnFormat', 0) == 1) $load_plugins .= "'UnFormat',";
+        $load_plugins = rtrim($load_plugins, ',');
 
         $return =  '<script type="text/javascript">' .
         '_editor_url  = "'.JURI::root().'plugins/editors/xinha/";' .
@@ -67,7 +87,7 @@ class plgEditorXinha extends JPlugin {
         'xinha_plugins = null;' .
         'xinha_init = xinha_init ? xinha_init : function() {' .
             'xinha_editors = xinha_editors ? xinha_editors : [\'text\'];' .
-            'xinha_plugins = xinha_plugins ? xinha_plugins : [\'CharacterMap\',\'ContextMenu\',\'ListType\',\'Stylist\',\'Linker\',\'SuperClean\',\'TableOperations\',\'ImageManager\'];' .
+            'xinha_plugins = xinha_plugins ? xinha_plugins : ['.$load_plugins.'];' .
             'if(Xinha.loadPlugins(xinha_plugins, xinha_init) == false) return;' .
             'xinha_config = xinha_config ? xinha_config : new Xinha.Config();' .
             'xinha_config.pageStyleSheets = [_editor_url + "examples/full_example.css"];' .
@@ -78,63 +98,27 @@ class plgEditorXinha extends JPlugin {
         '</script>';
 
         return $return;
-}
+    }
 
-    /**
-     * Xinha WYSIWYG Editor - get the editor content
-     *
-     * @param string    The name of the editor
-     */
-    function onGetContent( $editor ) {
-        //return "JContentEditor.getContent('".$editor."');";
+    function onGetContent($editor) {
         return "xinha_editors.$editor.getHTML();";
-
     }
 
-    /**
-     * Xinha WYSIWYG Editor - set the editor content
-     *
-     * @param string    The name of the editor
-     */
-    function onSetContent( $editor, $html ) {
-        //return "tinyMCE.activeEditor.setContent(".$html.");";
-        return "xinha_editors.$editor.setHTML(".$html.");";
+    function onSetContent($editor, $html) {
+        return "xinha_editors.$editor.setHTML($html);";
     }
 
-    /**
-     * Xinha WYSIWYG Editor - copy editor content to form field
-     *
-     * @param string    The name of the editor
-     */
     function onSave( $editor ) {
-        //return "tinyMCE.triggerSave();";
+        //TODO: make html cleanup
         return '"";';
     }
 
-    /**
-     * TinyMCE WYSIWYG Editor - display the editor
-     *
-     * @param string The name of the editor area
-     * @param string The content of the field
-     * @param string The width of the editor area
-     * @param string The height of the editor area
-     * @param int The number of columns for the editor area
-     * @param int The number of rows for the editor area
-     * @param mixed Can be boolean or array.
-     */
-    function onDisplay( $name, $content, $width, $height, $col, $row, $buttons = true) {
+    function onDisplay($name, $content, $width, $height, $col, $row, $buttons = true) {
         // Only add "px" to width and height if they are not given as a percentage
-        if (is_numeric( $width )) {
-            $width .= 'px';
-        }
-        if (is_numeric( $height )) {
-            $height .= 'px';
-        }
+        if(is_numeric($width)) $width .= 'px';
+        if(is_numeric($height)) $height .= 'px';
 
         $buttons = $this->_displayButtons($name, $buttons);
-        //$editor .= "<textarea id=\"$name\" name=\"$name\" cols=\"$col\" rows=\"$row\" style=\"width:{$width}; height:{$height};\" class=\"mceEditor\">$content</textarea>\n" . $buttons;
-        //$editor .= "<script type=\"text/javascript\">function jceOnLoad(){JContentEditor.initEditorMode('$name');}</script>";
-
         $editor = '<textarea name="'.$name.'" id="'.$name.'" cols="'.$col.'" rows="'.$row.'" style="width:'.$width.';height:'.$height.';">'.$content.'</textarea><br />'.$buttons;
 
         return $editor;
@@ -143,7 +127,6 @@ class plgEditorXinha extends JPlugin {
     function onGetInsertMethod($name) {
         $doc =& JFactory::getDocument();
 
-        //$js= "function jInsertEditorText( text, editor ) {tinyMCE.execInstanceCommand(editor, 'mceInsertContent',false,text);}";
         $js= "function jInsertEditorText(text, editor) {xinha_editors.$name.execCommand('insertHTML', false, text);}";
         $doc->addScriptDeclaration($js);
 
