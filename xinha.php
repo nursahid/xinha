d4a8e590ce7c2f25f8255085e4b14f8883164bde
edo888 <edo888@e0ad00cc-d266-11dd-a2b5-e5f827957e07>
2008-12-25 16:22:20 +0000

    Press release version

--- xinha.php
+++ xinha.php
@@ -49,118 +49,35 @@ class plgEditorXinha extends JPlugin {
         define('_XINHA_ISLOADED', 1);
 
         $plugin =& JPluginHelper::getPlugin('editors', 'xinha');
-        $params = new JParameter($plugin->params);
+        //$params = new JParameter($plugin->params);
+        $params = $plugin->params;
+        $language = $params->get('language', 'en');
+        $skin = $params->get('skin', '');
 
-        return <<<EOD
-<script type="text/javascript">
-_editor_url  = "/plugins/editors/xinha/"
-_editor_lang = "{$params->language}";
-_editor_skin = "{$params->skin}";
-</script>
-<script type="text/javascript" src="/plugins/editors/xinha/XinhaCore.js"></script>
-<script type="text/javascript">
-xinha_editors = null;
-xinha_init    = null;
-xinha_config  = null;
-xinha_plugins = null;
+        $return =  '<script type="text/javascript">' .
+        '_editor_url  = "'.JURI::root().'plugins/editors/xinha/";' .
+        '_editor_lang = "'.$language.'";' .
+        '_editor_skin = "'.$skin.'";' .
+        '</script>' .
+        '<script type="text/javascript" src="'.JURI::root().'plugins/editors/xinha/XinhaCore.js"></script>' .
+        '<script type="text/javascript">' .
+        'xinha_editors = null;' .
+        'xinha_init    = null;' .
+        'xinha_config  = null;' .
+        'xinha_plugins = null;' .
+        'xinha_init = xinha_init ? xinha_init : function() {' .
+            'xinha_editors = xinha_editors ? xinha_editors : [\'text\'];' .
+            'xinha_plugins = xinha_plugins ? xinha_plugins : [\'CharacterMap\',\'ContextMenu\',\'ListType\',\'Stylist\',\'Linker\',\'SuperClean\',\'TableOperations\',\'ImageManager\'];' .
+            'if(Xinha.loadPlugins(xinha_plugins, xinha_init) == false) return;' .
+            'xinha_config = xinha_config ? xinha_config : new Xinha.Config();' .
+            'xinha_config.pageStyleSheets = [_editor_url + "examples/full_example.css"];' .
+            'xinha_editors = Xinha.makeEditors(xinha_editors, xinha_config, xinha_plugins);' .
+            'Xinha.startEditors(xinha_editors);' .
+        '};' .
+        'Xinha.addOnloadHandler(xinha_init);' .
+        '</script>';
 
-// This contains the names of textareas we will make into Xinha editors
-xinha_init = xinha_init ? xinha_init : function() {
-/** STEP 1
- * First, specify the textareas that shall be turned into Xinhas.
- * For each one add the respective id to the xinha_editors array.
- * I you want add more than on textarea, keep in mind that these
- * values are comma seperated BUT there is no comma after the last value.
- * If you are going to use this configuration on several pages with different
- * textarea ids, you can add them all. The ones that are not found on the
- * current page will just be skipped.
- **/
-
-xinha_editors = xinha_editors ? xinha_editors : ['introtext', 'fulltext'];
-
-/** STEP 2
- * Now, what are the plugins you will be using in the editors on this
- * page.  List all the plugins you will need, even if not all the editors
- * will use all the plugins.
- *
- * The list of plugins below is a good starting point, but if you prefer
- * a simpler editor to start with then you can use the following
- *
- * xinha_plugins = xinha_plugins ? xinha_plugins : [ ];
- *
- * which will load no extra plugins at all.
- **/
-
-xinha_plugins = xinha_plugins ? xinha_plugins : ['CharacterMap','ContextMenu','ListType','Stylist','Linker','SuperClean','TableOperations','ImageManager'];
-
-// THIS BIT OF JAVASCRIPT LOADS THE PLUGINS, NO TOUCHING
-if(!Xinha.loadPlugins(xinha_plugins, xinha_init)) return;
-
-
-/** STEP 3
- * We create a default configuration to be used by all the editors.
- * If you wish to configure some of the editors differently this will be
- * done in step 5.
- *
- * If you want to modify the default config you might do something like this.
- *
- *   xinha_config = new Xinha.Config();
- *   xinha_config.width  = '640px';
- *   xinha_config.height = '420px';
- *
- **/
-
-xinha_config = xinha_config ? xinha_config() : new Xinha.Config();
-
-// To adjust the styling inside the editor, we can load an external stylesheet like this
-// NOTE : YOU MUST GIVE AN ABSOLUTE URL
-
-xinha_config.pageStyleSheets = [ _editor_url + "examples/full_example.css" ];
-
-/** STEP 4
- * We first create editors for the textareas.
- *
- * You can do this in two ways, either
- *
- *   xinha_editors   = Xinha.makeEditors(xinha_editors, xinha_config, xinha_plugins);
- *
- * if you want all the editor objects to use the same set of plugins, OR;
- *
- *   xinha_editors = Xinha.makeEditors(xinha_editors, xinha_config);
- *   xinha_editors.myTextArea.registerPlugins(['Stylist']);
- *   xinha_editors.anotherOne.registerPlugins(['CSS','SuperClean']);
- *
- * if you want to use a different set of plugins for one or more of the
- * editors.
- **/
-
-xinha_editors   = Xinha.makeEditors(xinha_editors, xinha_config, xinha_plugins);
-
-/** STEP 5
- * If you want to change the configuration variables of any of the
- * editors,  this is the place to do that, for example you might want to
- * change the width and height of one of the editors, like this...
- *
- *   xinha_editors.myTextArea.config.width  = '640px';
- *   xinha_editors.myTextArea.config.height = '480px';
- *
- **/
-
-
-/** STEP 6
- * Finally we "start" the editors, this turns the textareas into
- * Xinha editors.
- **/
-
-Xinha.startEditors(xinha_editors);
-
-}
-
-// this executes the xinha_init function on page load
-// and does not interfere with window.onload properties set by other scripts
-Xinha._addEvent(window,'load', xinha_init);
-</script>
-EOD;
+        return $return;
 }
 
     /**
@@ -170,7 +87,8 @@ EOD;
      */
     function onGetContent( $editor ) {
         //return "JContentEditor.getContent('".$editor."');";
-        return '';
+        return "xinha_editors.$editor.getHTML();";
+
     }
 
     /**
@@ -180,7 +98,7 @@ EOD;
      */
     function onSetContent( $editor, $html ) {
         //return "tinyMCE.activeEditor.setContent(".$html.");";
-        return '';
+        return "xinha_editors.$editor.setHTML(".$html.");";
     }
 
     /**
@@ -190,7 +108,7 @@ EOD;
      */
     function onSave( $editor ) {
         //return "tinyMCE.triggerSave();";
-        return '';
+        return '"";';
     }
 
     /**
@@ -214,20 +132,20 @@ EOD;
         }
 
         $buttons = $this->_displayButtons($name, $buttons);
-        $editor .= "<textarea id=\"$name\" name=\"$name\" cols=\"$col\" rows=\"$row\" style=\"width:{$width}; height:{$height};\" class=\"mceEditor\">$content</textarea>\n" . $buttons;
-        $editor .= "<script type=\"text/javascript\">function jceOnLoad(){JContentEditor.initEditorMode('$name');}</script>";
+        //$editor .= "<textarea id=\"$name\" name=\"$name\" cols=\"$col\" rows=\"$row\" style=\"width:{$width}; height:{$height};\" class=\"mceEditor\">$content</textarea>\n" . $buttons;
+        //$editor .= "<script type=\"text/javascript\">function jceOnLoad(){JContentEditor.initEditorMode('$name');}</script>";
 
-        return <<<EOD
-        <textarea name="$name" id="$name" cols="$col" rows="$row" style="width:$width;height:$height;">$content</textarea>
-        <br />$buttons
-        EOD;
+        $editor = '<textarea name="'.$name.'" id="'.$name.'" cols="'.$col.'" rows="'.$row.'" style="width:'.$width.';height:'.$height.';">'.$content.'</textarea><br />'.$buttons;
+
+        return $editor;
     }
 
     function onGetInsertMethod($name) {
-        $doc = & JFactory::getDocument();
+        $doc =& JFactory::getDocument();
 
         //$js= "function jInsertEditorText( text, editor ) {tinyMCE.execInstanceCommand(editor, 'mceInsertContent',false,text);}";
-        //$doc->addScriptDeclaration($js);
+        $js= "function jInsertEditorText(text, editor) {xinha_editors.$name.execCommand('insertHTML', false, text);}";
+        $doc->addScriptDeclaration($js);
 
         return true;
     }
@@ -247,8 +165,7 @@ EOD;
             }
         }
 
-        if(!empty($buttons))
-        {
+        if(!empty($buttons)) {
             $results = $this->_subject->getButtons($name, $buttons);
 
             /*
